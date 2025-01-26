89f3d28ad4159cb1266cbdf942dd0e2501cba64f
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-01-26 23:59:58 +0530

    Initiate Bad Request

--- src/main/java/lk/ijse/dep13/server/webweaver/WebWeaverServerApp.java
+++ src/main/java/lk/ijse/dep13/server/webweaver/WebWeaverServerApp.java
@@ -1,8 +1,9 @@
 package lk.ijse.dep13.server.webweaver;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class WebWeaverServerApp {
 
@@ -14,7 +15,43 @@ public class WebWeaverServerApp {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected from " + clientSocket.getRemoteSocketAddress());
 
+                new Thread(() -> {
+                    try {
+                        InputStream is = clientSocket.getInputStream();
+                        OutputStream os = clientSocket.getOutputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
 
+                        /* Reading the Command Line In the Request */
+                        String commandLine = br.readLine();
+                        if (commandLine == null || commandLine.isBlank()) {
+                            String response = """
+                                    HTTP/1.1 400 Bad Request
+                                    Server: web-weaver/0.1.0
+                                    Date: %s
+                                    Content-Type: text/html
+                                    
+                                    <!DOCTYPE html>
+                                    <html>
+                                    <head>
+                                    <title>Web Weaver | 400 Bad Request</title>
+                                    </head>
+                                    <body>
+                                    <h1>Web Weaver | 400 Bad Request</h1>
+                                    </body>
+                                    </html>
+                                    """.formatted(LocalDateTime.now());
+                            os.write(response.getBytes());
+                            os.flush();
+                            return;
+                        }
+
+
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
